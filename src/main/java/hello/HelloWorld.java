6ea9c69d3a680954007a0239229f3017c1eadf28
Michael Smith <Michael.Smith@vml.com>
2016-09-16 14:49:20 -0500

    Basic project setup with some Hello World stuff.

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,1 @@
+package hello;import org.joda.time.LocalTime;public class HelloWorld {  public static void main(String[] args) {    LocalTime currentTime = new LocalTime();    System.out.println("The time is " + currentTime);    System.out.println("Hello World!");  }}
